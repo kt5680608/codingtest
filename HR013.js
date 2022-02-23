427b64b8886de5cc539455c2f58b9eddcf845fe8
kt5680608 <gns7893@naver.com>
2022-02-23 18:03:25 +0900

    HR013

---
+++ HR013.js
@@ -0,0 +1,14 @@
+function kangaroo(x1, v1, x2, v2) {
+    while (true) {
+        if (v1 <= v2) {
+            return "NO";
+        }
+        x1 += v1;
+        x2 += v2;
+        if (x1 === x2) {
+            return "YES";
+        } else if (x1 > x2) {
+            return "NO";
+        }
+    }
+}
