ce93920c0ae0e185b02741a185f2de788a3b4565
kt5680608 <gns7893@naver.com>
2022-02-25 16:16:44 +0900

    HR023

---
+++ HR023.js
@@ -0,0 +1,10 @@
+// Cats and a Mouse
+function catAndMouse(x, y, z) {
+    if (Math.abs(x - z) < Math.abs(y - z)) {
+        return "Cat A";
+    } else if (Math.abs(x - z) > Math.abs(y - z)) {
+        return "Cat B";
+    } else {
+        return "Mouse C";
+    }
+}
