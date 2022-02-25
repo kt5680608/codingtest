e6845634d2461c015ae6983f899b9efbe1542659
kt5680608 <gns7893@naver.com>
2022-02-25 15:25:57 +0900

    HR017

---
+++ HR017.js
@@ -0,0 +1,21 @@
+function migratoryBirds(arr) {
+    //Migratory Birds
+    let count = 0;
+    let answer = 0;
+    arr.sort((a, b) => a - b);
+    for (let i = 0; i < arr.length; i++) {
+        if (count < arr.lastIndexOf(arr[i]) - arr.indexOf(arr[i]) + 1) {
+            answer = arr[i];
+            count = arr.lastIndexOf(arr[i]) - arr.indexOf(arr[i]) + 1;
+            // i를 전부 검색하면 time limit에 걸려서 같은 값을 제외하고 검색
+            i = arr.lastIndexOf(arr[i]) + 1;
+        } else if (
+            count ===
+            arr.lastIndexOf(arr[i]) - arr.indexOf(arr[i]) + 1
+        ) {
+            i = arr.lastIndexOf(arr[i]) + 1;
+            continue;
+        }
+    }
+    return answer;
+}
