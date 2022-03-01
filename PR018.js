80197f2234f44b3234f85fcd56c9a76f02c3a3f4
kt5680608 <gns7893@naver.com>
2022-03-01 16:01:49 +0900

    PR018

---
+++ PR018.js
@@ -0,0 +1,12 @@
+// 같은 숫자는 싫어
+function solution(arr) {
+    let answer = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i + 1]) {
+            continue;
+        } else {
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
