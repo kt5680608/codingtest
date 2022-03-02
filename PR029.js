2f385abd7f8532e52d085fe1f07b5d82678ece91
kt5680608 <gns7893@naver.com>
2022-03-02 14:59:47 +0900

    PR029

---
+++ PR029.js
@@ -0,0 +1,18 @@
+// 약수의 개수와 덧셈
+function solution(left, right) {
+    let sum = 0;
+    for (let i = left; i <= right; i++) {
+        let count = 0;
+        for (let j = 1; j <= i; j++) {
+            if (i % j === 0) {
+                count++;
+            }
+        }
+        if (count % 2 === 0) {
+            sum += i;
+        } else {
+            sum -= i;
+        }
+    }
+    return sum;
+}
