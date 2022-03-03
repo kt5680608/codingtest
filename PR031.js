d06aeddc341f5b5c49df0725725401b806bb77b2
kt5680608 <gns7893@naver.com>
2022-03-03 17:31:38 +0900

    PR031

--- PR031.js
+++ PR031.js
@@ -1,7 +1,4 @@
 // 3진법 뒤집기
 function solution(n) {
-    n = n.toString(3).split("");
-    n = n.reverse().join("");
-    n = Number.parseInt(n, 3);
-    return n;
+    return Number.parseInt(n.toString(3).split("").reverse().join(""), 3);
 }
