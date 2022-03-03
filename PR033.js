937161a62063d783f8b7d69082a136bca1602422
kt5680608 <gns7893@naver.com>
2022-03-03 17:44:28 +0900

    PR033

--- PR033.js
+++ PR033.js
@@ -1,4 +1,3 @@
-// 영어 끝말잇기
 function solution(n, words) {
     let count = 1;
     let answer = [];
@@ -10,14 +9,9 @@ function solution(n, words) {
         let memberCount = Math.ceil(count / n);
         if (member === 0) member = n;
         if (member > n) member = 1;
+
+        // 끝말잇기 실패 or 단어 중복 startwith로 바꾸기
         if (
-            word[word.length - 1] === wordNext[0] &&
-            words.indexOf(words[i]) === i &&
-            words.indexOf(words[i + 1]) === i + 1
-        ) {
-        }
-        // 끝말잇기 실패 or 단어 중복
-        else if (
             word[word.length - 1] !== wordNext[0] ||
             words.indexOf(words[i + 1]) !== i + 1
         ) {
