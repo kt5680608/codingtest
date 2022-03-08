2e2e69ca3a04fa2a87eb4f90c0a505cc42bbcce0
kt5680608 <gns7893@naver.com>
2022-03-08 16:10:05 +0900

    PR050

---
+++ PR050.js
@@ -0,0 +1,34 @@
+// 숫자 문자열과 영단어
+function solution(s) {
+    let alphabet = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    let finder = [];
+    let answer = [];
+    var checkEng = /[a-zA-Z]/;
+    for (let i = 0; i < s.length; i++) {
+        if (checkEng.test(s[i]) === true) {
+            finder.push(s[i]);
+            let string = finder.join("");
+            console.log(string);
+            if (alphabet.indexOf(string) !== -1) {
+                answer.push(alphabet.indexOf(string));
+                finder = [];
+                string = "";
+            }
+            finder = string.split("");
+        } else {
+            answer.push(s[i]);
+        }
+    }
+    return Number(answer.join(""));
+}
